6d9f1ac193bd3431199e99b31984fd15db025570
0xadewale <crypto.adewale@gmail.com>
2022-12-01 18:33:17 +0100

    new package info

--- next.config.js
+++ next.config.js
@@ -1,7 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  productionBrowserSourceMaps: true,
+  images: {
+    domains: [
+      'ipfs.infura.io',
+      'statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com',
+      'lens.infura-ipfs.io'
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: 'https://sitemap.lenstats.xyz/sitemap.xml'
+      },
+      {
+        source: '/sitemaps/:match*',
+        destination: 'https://sitemap.lenstats.xyz/sitemaps/:match*'
+      }
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+          { key: 'Referrer-Policy', value: 'strict-origin' },
+          { key: 'Permissions-Policy', value: 'interest-cohort=()' }
+        ]
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
